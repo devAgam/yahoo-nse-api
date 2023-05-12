44bcaa15dd9c8ec802c6853b4d644d04246ead84
devAgam <tippytactics@gmail.com>
2023-05-11 18:03:45 -0700

    add functions and types

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-#NSE Quotes API
+# NSE Quotes API
+
+![npm version](https://img.shields.io/npm/v/nse-quotes-api)
+
+I made this wrapper for easing my personal projects. It is a simple wrapper for the Yahoo Finance API. It is not an official API.
+
+## Features
+
+- Get latest quotes of single NSE stock ✨.
+- Get latest quotes of `multiple` NSE stocks 🚀.
+- Typescript support 🎉.
+
+## Installation
+
+```bash
+npm install nse-quotes-api
+```
+
+## Usage
+
+```js
+const { getMultipleLTPs } = require("nse-quotes-api");
+
+// Get quotes of single stock
+const data = getLTP("INFY");
+
+// Get quotes of multiple stocks
+const data = getMultipleLTPs(["TCS", "INFY"]);
+```
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { YahooQuoteResponse, YahooQuoteResult } from "./yahoo.quotes.types";
+import { RequestInfo, RequestInit } from "node-fetch";
+
+const fetch = (url: RequestInfo, init?: RequestInit) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(url, init));
+
+export async function getMultipleLTPs(
+  symbols: string[]
+): Promise<YahooQuoteResponse> {
+  const baseV6YahooFinanceURL =
+    "https://query1.finance.yahoo.com/v6/finance/quote?symbols=";
+  // add .NS to each symbol
+  const symbolsWithNSE = symbols.map((symbol) => symbol + ".NS");
+  const symbolsString = symbolsWithNSE.join(",");
+  const url = baseV6YahooFinanceURL + symbolsString;
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+}
+
+export async function getLTP(symbol: string): Promise<YahooQuoteResult> {
+  const baseV6YahooFinanceURL =
+    "https://query1.finance.yahoo.com/v6/finance/quote?symbols=";
+  const symbolWithNSE = symbol + ".NS";
+  const url = baseV6YahooFinanceURL + symbolWithNSE;
+  const response = await fetch(url);
+  const json = await response.json();
+  if (json.quoteResponse.result.length === 0) {
+    throw new Error(`No result found for ${symbol}`);
+  }
+  return json.quoteResponse.result[0];
+}
